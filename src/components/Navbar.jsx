78488cf161188b7e00e0fee5ab6a301eb36d4a50
ibktommy <atomquartz@gmail.com>
2022-07-13 19:02:27 +0100

    Complete the theme-light/dark mode toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ThemeToggleBtn from "./ThemeToggleBtn";
 
 const Navbar = () => {
 	return (
 		<div>
 			<Link to="/">
-				<h1>CrytoVerse</h1>
+				<h1>Cryptonetary</h1>
 			</Link>
+			<div>
+				<ThemeToggleBtn />
+			</div>
 		</div>
 	);
 };
