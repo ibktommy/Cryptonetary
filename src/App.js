5f7c16b9cc1e7313cbb7a05ef776f51ade23192e
ibktommy <atomquartz@gmail.com>
2022-07-28 13:52:39 +0100

    Create State to monitior the data returned from api and make request to the api server in the useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
@@ -6,8 +6,25 @@ import Home from "./routes/Home";
 import Login from './routes/Login'
 import Register from './routes/Register'
 import Account from './routes/Account'
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([])
+
+  const apiURL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+
+  // Making API request after the component re-renders and set the dependency to monitor changes to the apiURL
+  useEffect(() => {
+    axios.get(apiURL)
+      .then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
+  }, [apiURL])
+
   return (
     <ThemeProvider>
       <Navbar />
