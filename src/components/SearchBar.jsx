42b4c678426565c1ced8e01486ce443e31d24443
ibktommy <atomquartz@gmail.com>
2022-07-29 23:15:41 +0100

    Using Array Filter method to return a new dataItem array based on the condition set each time a value-change happens on the input-field-text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 import DataItem from "./DataItem";
 
 const SearchBar = ({ data }) => {
 	console.log(data);
+
+	const [searchText, setSearchText] = useState("");
 
 	return (
 		<div>
 			<div>
 				<h1>Search Crypto</h1>
 				<form>
-					<input type="text" placeholder="Search a Coin" />
+					<input
+						type="text"
+						placeholder="Search a Coin"
+						onChange={(e) => setSearchText(e.target.value)}
+					/>
 				</form>
 			</div>
 
@@ -27,13 +33,23 @@ const SearchBar = ({ data }) => {
 				</thead>
 
 				<tbody>
-					{data.map((dataItem) => (
-						<DataItem key={dataItem.id} dataItem={dataItem}/>
-					))}
+					{data
+						.filter((value) => {
+							if (searchText === "") {
+								return value;
+							} else if (
+								value.name.toLowerCase().includes(searchText.toLowerCase())
+							) {
+								return value;
+							}
+						})
+						.map((dataItem) => (
+							<DataItem key={dataItem.id} dataItem={dataItem} />
+						))}
 				</tbody>
 			</table>
 		</div>
 	);
 };
 
-export default SearchBar
+export default SearchBar;
