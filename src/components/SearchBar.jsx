3280a1e41dd5ca0357eac89e79a61283bf0cc14f
ibktommy <atomquartz@gmail.com>
2022-07-29 23:21:43 +0100

    Styling of the SearchBar Component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,11 +7,12 @@ const SearchBar = ({ data }) => {
 	const [searchText, setSearchText] = useState("");
 
 	return (
-		<div>
-			<div>
-				<h1>Search Crypto</h1>
+		<div className="rounded-div my-4">
+			<div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+				<h1 className="text-2xl font-bold my-2">Search Crypto</h1>
 				<form>
 					<input
+						className="w-full bg-primary border-input px-4 py-2 rounded-2xl shadow-xl"
 						type="text"
 						placeholder="Search a Coin"
 						onChange={(e) => setSearchText(e.target.value)}
@@ -19,16 +20,16 @@ const SearchBar = ({ data }) => {
 				</form>
 			</div>
 
-			<table>
+			<table className="w-full border-collapse text-center">
 				<thead>
-					<th></th>
-					<th>#</th>
+					<th className="px-4"></th>
+					<th className="text-left">#</th>
 					<th>Coin</th>
 					<th></th>
 					<th>Price</th>
 					<th>24hr</th>
-					<th>24hr Volume</th>
-					<th>Mkt</th>
+					<th className="hidden md:table-cell">24hr Volume</th>
+					<th className="hidden md:table-cell">Mkt</th>
 					<th>Last 7 Days</th>
 				</thead>
 
