32658eb7e0181637be2133d59644b91621f86a20
ibktommy <atomquartz@gmail.com>
2022-07-31 21:37:47 +0100

    Create TrendingComponent and import it in the HomeComponent

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import Trending from "../components/Trending";
 
 const Home = ({ data }) => {
-	return <SearchBar data={data}/>;
+	return (
+		<>
+			<SearchBar data={data} />
+			<Trending />
+		</>
+	);
 };
 
 export default Home;
