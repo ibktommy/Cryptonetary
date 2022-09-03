83870f09db65b7a68b93057bf4b85457043252fb
ibktommy <atomquartz@gmail.com>
2022-09-03 15:05:39 +0100

    Use Array for the savedCoins-state

--- src/components/SavedCoin.jsx
+++ src/components/SavedCoin.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SavedCoin = () => {
+	const [savedCoins, setSavedCoins] = useState([]);
+
 	return (
 		<div>
-			<p>
-				You don't have any coins saved. Please save a coin to add it to the
-				Watchlist.{" "}
-				<Link to="/" className="text-red-500">
-					Click here to Search Coins.
-				</Link>
-			</p>
+			{savedCoins.length === 0 ? (
+				<p>
+					You don't have any coins saved. Please save a coin to add it to the
+					Watchlist.{" "}
+					<Link to="/" className="text-red-500">
+						Click here to Search Coins.
+					</Link>
+				</p>
+			) : (
+				<table>
+					<thead>
+						<tr>
+							<th>Rank</th>
+							<th>Coin</th>
+							<th>Remove</th>
+						</tr>
+					</thead>
+					<tbody>
+						{savedCoins.map((coin) => {
+							return (
+								<tr key={coin?.id}>
+									<td>{coin?.rank}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			)}
 		</div>
 	);
 };
