caef91114ea501204223212db09953b9059b8c6f
ibktommy <atomquartz@gmail.com>
2022-09-03 13:31:09 +0100

    Add elements to the Account Page

---
+++ src/components/SavedCoin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SavedCoin = () => {
+	return <div>SavedCoin</div>;
+};
+
+export default SavedCoin;
--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Account = () => {
-	return <div>Account</div>;
+	return (
+		<div>
+			<div>
+				<div>
+					<h1>Account</h1>
+					<div>
+						<p>Welcome, User</p>
+					</div>
+				</div>
+				<div>
+					<button>Logout</button>
+				</div>
+			</div>
+			<div>
+				<div>
+					<h1>Watch List</h1>
+				</div>
+			</div>
+		</div>
+	)
 };
 
 export default Account;
