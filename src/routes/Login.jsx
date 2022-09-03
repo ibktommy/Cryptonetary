0fac9a243c858c84f9ddd662c6c6455a55e139bf
ibktommy <atomquartz@gmail.com>
2022-09-03 13:14:08 +0100

    Add Styles to the Login Page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -5,27 +5,41 @@ import { Link } from "react-router-dom";
 const Login = () => {
 	return (
 		<div>
-			<div
-			>
-				<h1>Login</h1>
+			<div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
+				<h1 className="text-2xl font-bold">Login</h1>
 				<form>
-					<div>
+					<div className="my-4">
 						<label htmlFor="email">Email</label>
-						<div>
-							<input type="email" id="email" />
-							<AiOutlineMail />
+						<div className="my-2 w-full relative rounded-2xl shadow-xl">
+							<input
+								className="w-full p-2 bg-primary border border-input rounded-2xl"
+								type="email"
+								id="email"
+							/>
+							<AiOutlineMail className="absolute right-2 top-3 text-gray-400" />
 						</div>
 					</div>
-					<div>
+					<div className="my-4">
 						<label htmlFor="password">Password</label>
-						<div>
-							<input type="password" name="password" />
-							<AiFillLock />
+						<div className="my-2 w-full relative rounded-2xl shadow-xl">
+							<input
+								className="w-full p-2 bg-primary border border-input rounded-2xl"
+								type="password"
+								name="password"
+							/>
+							<AiFillLock className="absolute right-2 top-3 text-gray-400" />
 						</div>
 					</div>
-					<button>Login</button>
+					<button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+						Login
+					</button>
 				</form>
-				<p>Don't have an account? <Link to=''>Register An Account</Link></p>
+				<p className="my-4">
+					Don't have an account?{" "}
+					<Link to="/register" className=" text-red-400 ml-2">
+						Register An Account
+					</Link>
+				</p>
 			</div>
 		</div>
 	);
