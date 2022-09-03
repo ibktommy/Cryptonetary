f757fefadd0ee6cb2212c50061d26f155ef2db48
ibktommy <atomquartz@gmail.com>
2022-09-03 12:58:56 +0100

    Adding elements to the Login page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { AiOutlineMail, AiFillLock } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div>
+			<div
+			>
+				<h1>Login</h1>
+				<form>
+					<div>
+						<label htmlFor="email">Email</label>
+						<div>
+							<input type="email" id="email" />
+							<AiOutlineMail />
+						</div>
+					</div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<div>
+							<input type="password" name="password" />
+							<AiFillLock />
+						</div>
+					</div>
+					<button>Login</button>
+				</form>
+				<p>Don't have an account? <Link to=''>Register An Account</Link></p>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
