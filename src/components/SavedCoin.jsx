fe794e48bf6cdc2d15062a21ddface5cbc7b3661
ibktommy <atomquartz@gmail.com>
2022-09-03 18:16:17 +0100

    Styling the Accounts Page

--- src/components/SavedCoin.jsx
+++ src/components/SavedCoin.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedCoin = () => {
-	const [savedCoins, setSavedCoins] = useState([]);
+	const [savedCoins, setSavedCoins] = useState([1]);
 
 	return (
 		<div>
@@ -15,19 +16,39 @@ const SavedCoin = () => {
 					</Link>
 				</p>
 			) : (
-				<table>
+				<table className="w-full border-collapse text-center">
 					<thead>
-						<tr>
-							<th>Rank</th>
-							<th>Coin</th>
-							<th>Remove</th>
+						<tr className="border-b">
+							<th className="px-4">Rank</th>
+							<th className="text-left">Coin</th>
+							<th className="text-left">Remove</th>
 						</tr>
 					</thead>
 					<tbody>
 						{savedCoins.map((coin) => {
 							return (
-								<tr key={coin?.id}>
+								<tr key={coin?.id} className="h-[60px] overflow-hidden">
 									<td>{coin?.rank}</td>
+									<td>
+										<Link to={`/coin/${coin.id}`}>
+											<div className="flex items-center">
+												<img
+													src={coin?.name}
+													alt={coin?.name}
+													className="w-8 mr-4"
+												/>
+												<div>
+													<p className="hidden sm:table-cell">{coin?.name}</p>
+													<p className="text-gray-500 text-left text-sm">
+														{coin?.symbol?.toUpperCase()}
+													</p>
+												</div>
+											</div>
+										</Link>
+									</td>
+									<td className="pl-8">
+										<AiOutlineClose className="cursor-pointer" />
+									</td>
 								</tr>
 							);
 						})}
--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -3,21 +3,23 @@ import SavedCoin from "../components/SavedCoin";
 
 const Account = () => {
 	return (
-		<div>
-			<div>
+		<div className="max-w-[1140px] mx-auto">
+			<div className="flex justify-between items-center my-12 py-8 rounded-div">
 				<div>
-					<h1>Account</h1>
+					<h1 className="text-2xl font-bold">Account</h1>
 					<div>
 						<p>Welcome, User</p>
 					</div>
 				</div>
 				<div>
-					<button>Logout</button>
+					<button className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">
+						Logout
+					</button>
 				</div>
 			</div>
-			<div>
-				<div>
-					<h1>Watch List</h1>
+			<div className="flex justify-between items-center my-12 py-8 rounded-div">
+				<div className="w-full min-h-[300px]">
+					<h1 className="text-2xl font-bold py-4">Watch List</h1>
 					<SavedCoin />
 				</div>
 			</div>
