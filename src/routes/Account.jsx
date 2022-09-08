e3c2e1057290061c6e1bc96b2bac1b351c630083
ibktommy <atomquartz@gmail.com>
2022-09-08 17:02:59 +0100

    Add condition that only displays the Account page when userAuth is valid

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import SavedCoin from "../components/SavedCoin";
 import { UserAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
@@ -19,32 +19,36 @@ const Account = () => {
 		}
 	};
 
-	return (
-		<div className="max-w-[1140px] mx-auto">
-			<div className="flex justify-between items-center my-12 py-8 rounded-div">
-				<div>
-					<h1 className="text-2xl font-bold">Account</h1>
+	if (user) {
+		return (
+			<div className="max-w-[1140px] mx-auto">
+				<div className="flex justify-between items-center my-12 py-8 rounded-div">
 					<div>
-						<p>Welcome, {user?.email}</p>
+						<h1 className="text-2xl font-bold">Account</h1>
+						<div>
+							<p>Welcome, {user?.email}</p>
+						</div>
+					</div>
+					<div>
+						<button
+							className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
+							onClick={logOutHandler}
+						>
+							Logout
+						</button>
 					</div>
 				</div>
-				<div>
-					<button
-						className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
-						onClick={logOutHandler}
-					>
-						Logout
-					</button>
+				<div className="flex justify-between items-center my-12 py-8 rounded-div">
+					<div className="w-full min-h-[300px]">
+						<h1 className="text-2xl font-bold py-4">Watch List</h1>
+						{user?.email && <SavedCoin />}
+					</div>
 				</div>
 			</div>
-			<div className="flex justify-between items-center my-12 py-8 rounded-div">
-				<div className="w-full min-h-[300px]">
-					<h1 className="text-2xl font-bold py-4">Watch List</h1>
-					{user?.email && <SavedCoin />}
-				</div>
-			</div>
-		</div>
-	);
+		);
+	} else {
+		return <Navigate to="/login" />;
+	}
 };
 
 export default Account;
