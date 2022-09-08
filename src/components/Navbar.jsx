fddf0c7de2c5a9dd16c76894119e9dd6871bded2
ibktommy <atomquartz@gmail.com>
2022-09-08 11:40:34 +0100

    Change the rendering of Button On The Navbar Based on If User-Auth is valid/Not Valid

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ThemeToggleBtn from "./ThemeToggleBtn";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
 	// Creating State to Monitior Menu-Icon Click
 	const [nav, setNav] = useState(false);
+
+	// Destructuring Values from AuthContext
+	const { user, logout } = UserAuth();
+	const navigate = useNavigate();
+
+	// Function to Handle Logout Button
+	const logOutHandler = async () => {
+		try {
+			await logout();
+			navigate("/");
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
 	// Function to Modify the nav-state
 	function toggleNavIcon() {
@@ -22,17 +37,31 @@ const Navbar = () => {
 				<ThemeToggleBtn />
 			</div>
 
-			<div className="hidden md:block">
-				<Link to="/login" className="p-4 hover:text-accent">
-					Login
-				</Link>
-				<Link
-					to="/register"
-					className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
-				>
-					Register
-				</Link>
-			</div>
+			{/* Change the rendering of Button On The Navbar Based on If User-Auth is valid/Not Valid */}
+			{user?.email ? (
+				<div>
+					<Link to="/account" className="p-4">
+						Account
+					</Link>
+					<button className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl">
+						Logout
+					</button>
+				</div>
+			) : (
+				<div className="hideen md:block">
+					<div className="hidden md:block">
+						<Link to="/login" className="p-4 hover:text-accent">
+							Login
+						</Link>
+						<Link
+							to="/register"
+							className="bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl"
+						>
+							Register
+						</Link>
+					</div>
+				</div>
+			)}
 
 			{/* MENU ICON FOR MOBILE DEVICES */}
 			<div
