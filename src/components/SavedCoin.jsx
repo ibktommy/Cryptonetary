bdb8c8df885a84bc4fd6188630d82ee5fca38170
ibktommy <atomquartz@gmail.com>
2022-09-08 13:39:36 +0100

    Create Funtion that deletes a selected coin from the Firestore-Database and also filter out that coin and then updates the WatchList-Array with the Coins that are left

--- src/components/SavedCoin.jsx
+++ src/components/SavedCoin.jsx
@@ -6,15 +6,30 @@ import { UserAuth } from "../context/AuthContext";
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 
 const SavedCoin = () => {
-	const [savedCoins, setSavedCoins] = useState([0]);
+	const [savedCoins, setSavedCoins] = useState([]);
 	const { user } = UserAuth();
 
 	// Getting the Data-items in the Firestore Database with useEffect After Component renders
 	useEffect(() => {
-		onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+		onSnapshot(doc(db, "users", `${user.email}`), (doc) => {
 			setSavedCoins(doc.data()?.watchList);
 		});
-	}, [user?.email]);
+	}, [user.email]);
+
+	// Funtion to Delete An item from the setSavedCoin-state-Array
+	const coinPath = doc(db, "users", `${user?.email}`); //Getting Path of Item in Database
+	const deleteCoin = async (savedCoinID) => {
+		try {
+			const filteredavedCoins = savedCoins.filter(
+				(coin) => coin.id !== savedCoinID,
+			);
+			await updateDoc(coinPath, {
+				watchList: filteredavedCoins,
+			});
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
 	return (
 		<div>
@@ -57,7 +72,7 @@ const SavedCoin = () => {
 											</div>
 										</Link>
 									</td>
-									<td className="pl-8">
+									<td className="pl-8" onClick={() => deleteCoin(coin.id)}>
 										<AiOutlineClose className="cursor-pointer" />
 									</td>
 								</tr>
